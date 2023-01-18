1f977d7d81bcfefba3750f6908b28a1d4f132477
Pedro Miguel <pedromrap@gmail.com>
2023-01-17 21:07:52 -0300

    feat: criação da rotas e adicionando o prisma na pasta lib

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,16 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client';
+import { appRoutes } from './routes';
 
 const app = Fastify();
-const prisma = new PrismaClient();
 const PORT = 3333;
 
 app.register(cors, {
   origin: 'https://localhost:3000'
 })
+app.register(appRoutes)
 
-app.get('/', async (req, res) => {
- const habits = await prisma.habit.findMany()
 
-  return habits
-});
 
 app.listen({
   port: PORT,
